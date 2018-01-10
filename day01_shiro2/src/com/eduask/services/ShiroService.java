ba480b5b0bd5bff72ba1b5fbc23b911beeb7b612
wxf <1281368854@qq.com>
2018-01-10 15:30:31 +0800

    upload shiro_01

---
+++ day01_shiro2/src/com/eduask/services/ShiroService.java
@@ -0,0 +1,20 @@
+package com.eduask.services;
+
+import java.util.Date;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+
+public class ShiroService {
+	//此时用user登录，访问此方法会报错，可以通过spring的注解异常处理，返回到异常页面
+	@RequiresRoles("admin")
+	public void testMethod(){
+		System.out.println("我是shiro的service层:"+new Date());
+		Subject subject = SecurityUtils.getSubject(); 
+		Session session =subject.getSession();
+		Object val= session.getAttribute("wxl");
+		System.out.println(val);
+	}
+}
